283d574063fba3e7552449195be2c41e028b84a1
zikzakcreatef-boop <zikzak.createf@gmail.com>
2026-02-02 09:58:43 +0900

    1.05

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // config.js
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwnzSSz9eqsgce0EpmdCAQm_79vcUjGDPKzLGPAHEZs6mKkr0APEZUr5cnSzMOOBTmUNw/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxlDgLkmJP1eJ1OrIeoOkYozTeya1TBq4aJk7HoCsx-JJXULPLtv5j6e68_-5JwZmd-Hw/exec";
+const API_KEY = "Z2VtX21heF8yMDI2X3NlY3VyZQ=="; // 合言葉を追記
