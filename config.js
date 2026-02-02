cae9c1367ba5e1a6994722949be9bb4a6bb49a21
zikzakcreatef-boop <zikzak.createf@gmail.com>
2026-02-02 11:03:58 +0900

    secure update

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxlDgLkmJP1eJ1OrIeoOkYozTeya1TBq4aJk7HoCsx-JJXULPLtv5j6e68_-5JwZmd-Hw/exec";
-const API_KEY = "Z2VtX21heF8yMDI2X3NlY3VyZQ=="; // 合言葉を追記
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwc7yVBg23xUGORBoQZlI0vZAw9UU6tDl1z-e_kWVKZr8aE7_M8yy47vQSn7TUal1kC3g/exec";
+const API_KEY = "Z2VtX21heF8yMDI2X3NlY3VyZQ==";
