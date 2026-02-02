b7e7f269a82cbc65e357de05547af47ef7712a72
zikzakcreatef-boop <zikzak.createf@gmail.com>
2026-02-02 11:59:37 +0900

    ui

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-const GAS_URL = "https://script.google.com/macros/s/AKfycbw2o0CxCFLogxRYKOBrHgIS1SmyQZeW5aV-FPmhaOhxGNFz51CeIVgrpGN_98ReUOHHwA/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzXIqbT0MiD4d2Wr9aFH4191BBP6tMDu1szY_YIVtG9KsKOc6MFRqnGK-cKHVi3689ApQ/exec";
 const API_KEY = "Z2VtX21heF8yMDI2X3NlY3VyZQ==";
