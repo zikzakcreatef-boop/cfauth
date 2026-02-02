e788c5dcbada13461f1c03aba32b9b455f776118
zikzakcreatef-boop <zikzak.createf@gmail.com>
2026-02-02 11:13:58 +0900

    debug

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwc7yVBg23xUGORBoQZlI0vZAw9UU6tDl1z-e_kWVKZr8aE7_M8yy47vQSn7TUal1kC3g/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzog5n6hGExUWKbUQcvzDKSb9-kFsUvObifgJw4KfO0NEpq21slJ7DAfFBpnDJUBCih7A/exec";
 const API_KEY = "Z2VtX21heF8yMDI2X3NlY3VyZQ==";
