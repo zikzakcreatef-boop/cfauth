93ef605a8bbb82d2d0888140e3bc791bb4dedd34
zikzakcreatef-boop <zikzak.createf@gmail.com>
2026-02-05 13:16:27 +0900

    1.14
    メール復活

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
  */
 
 // GASのウェブアプリURL（デプロイ後に取得したもの）
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxShzS4lYvocuF6QnFJoVVxjvezNdmR9E-OyPzjXxsvlMC4YPX0F5Db9S1p1ac1sp-l/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxFSA8o7KUPRWyqEULBH0D0F8ybdbZUPTzsdkX6K6whlkW8-gwybslZLi7cM_tJxVpbgg/exec";
 
 // スプレッドシートの config シートに設定した 'api_key' と一致させること
 const API_KEY = "Z2VtX21heF8yMDI2X3NlY3VyZQ==";
