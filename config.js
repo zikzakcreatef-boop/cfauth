3088cd939fcd0a0d1a4ec5cdf1fa9b65b1c98450
zikzakcreatef-boop <zikzak.createf@gmail.com>
2026-02-05 09:58:33 +0900

    1.11
    ログイン後の画面から、システム全体の説明文（2行目のdescription）を非表示に。
    
    リストアイテムから各個別の description を削除（継承）。
    
    既読時のリアルタイム・カウントダウン同期機能を維持。

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
  */
 
 // GASのウェブアプリURL（デプロイ後に取得したもの）
-const GAS_URL = "https://script.google.com/macros/s/AKfycbyAjyf43bXjVki9NuHJSCceCY5KGky9fqCw1Q_ZW-7650eC2eOzb1laHEEZAXIY2inxqw/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyFsMQTACvyFwHLIWhKyHnHdVQabuu4guslC85hsjzfbobvuJrJ3rOZcf9Oxz0BDmgdww/exec";
 
 // スプレッドシートの config シートに設定した 'api_key' と一致させること
 const API_KEY = "Z2VtX21heF8yMDI2X3NlY3VyZQ==";
