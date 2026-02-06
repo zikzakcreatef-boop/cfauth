31a06f6a0b3af3b6a8d94e692fded4a2c07bc6d9
zikzakcreatef-boop <zikzak.createf@gmail.com>
2026-02-06 09:05:04 +0900

    1.41

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
  */
 
 // GASのウェブアプリURL（デプロイ後に取得したもの）
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwgshfPqDEvPZfsle2bMca8fLauVh9JYT9A7zVrprmOm9KamQRyfypPhJWkMnNJgAIn0Q/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbx41I9yJUuX4eM2fs4Z5VcVpPL2dNGpObN-s8s0TTKj1yiwdS3q1-uQjPvrI21Sz55tHg/exec";
 
 // スプレッドシートの config シートに設定した 'api_key' と一致させること
 const API_KEY = "Z2VtX21heF8yMDI2X3NlY3VyZQ==";
